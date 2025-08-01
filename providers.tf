f604a3e9521eef5c989f6626be751bd9c6cd2fea
diogo-nogueira-freiheit <diogo.nogueira@freiheit.com>
2025-08-01 15:28:51 +0100

    Fix versions

--- providers.tf
+++ providers.tf
@@ -2,23 +2,23 @@ terraform {
   required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = "~> 1.19.0"
+      version = "1.19.0"
     }
     vault = {
       source  = "hashicorp/vault"
-      version = "~> 4.3.0"
+      version = "4.3.0"
     }
     stackit = {
       source  = "stackitcloud/stackit"
-      version = "~> 0.54.0"
+      version = "0.54.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = "2.38.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.2"
+      version = "4.38.1"
     }
     solacebroker = {
       source  = "SolaceProducts/solacebroker"
@@ -26,11 +26,11 @@ terraform {
     }
     datadog = {
       source  = "DataDog/datadog"
-      version = "~> 3.0"
+      version = "3.69.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.4"
+      version = "2.5.3"
     }
   }
 }
